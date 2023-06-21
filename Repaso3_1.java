5539919b4aa212dc4bde9953b99b4934f647f8a6
Maximiliano D <mdd.bsd@gmail.com>
2023-06-21 11:19:41 -0300

    Agregadas mas funciones al ejercicio stocks Repaso3_1

--- Repaso3_1.java
+++ Repaso3_1.java
@@ -5,24 +5,29 @@ public class Repaso3_1 {
                                {1290, 8, 0},
                                {800,  5, 0}};
         int op;
-        op = MostrarMenu(); //llamada  a la funcion menu
-        while(op < 1 || op > 3){
-            System.out.println("Opcion incorrecta, ingrese nuevamente");
-            op = MostrarMenu();
-        }
-        switch(op){
-            case 1:
-                MuestraStocks(prods, prodsVal);
-                break;
-            case 2:
-                MuestraCostos(prods, prodsVal);
-                break;
-            case 3:
-                MuestraStockCostos(prods, prodsVal);
-                break;
-            default:
-
-        } 
+        boolean flag = true;
+        do{
+            op = MostrarMenu(); //llamada  a la funcion menu
+            while(op < 1 || op > 4){
+                System.out.println("Opcion incorrecta, ingrese nuevamente");
+                op = MostrarMenu();
+            }
+            switch(op){
+                case 1:
+                    MuestraStocks(prods, prodsVal);
+                    break;
+                case 2:
+                    CalculaCostos(prodsVal);
+                    System.out.println("Costos totales calculados");
+                    break;
+                case 3:
+                    MuestraTotales(prods, prodsVal);
+                    break;
+                case 4:
+                    flag = false;
+                    break;
+            }
+        }while(flag);
     }//TERMINA LA FUNCION MAIN
 
     //CONMIENZA LA DECLARACION  DE FUNCIONES
@@ -38,6 +43,7 @@ public class Repaso3_1 {
         System.out.println("1: mostrar productos y stock");
         System.out.println("2: calcular costos");
         System.out.println("3: mostrar costos totales");
+        System.out.println("4: Salir");
         return Integer.parseInt(System.console().readLine());
     }
     
@@ -53,4 +59,19 @@ public class Repaso3_1 {
         }
     }
 
+    public static void CalculaCostos(double marcelo[][]){
+        for(int i = 0; i < 3; i++){
+            marcelo[i][2] = marcelo[i][0] * marcelo[i][1];        
+        }
+    }
+
+    public static void MuestraTotales(String prods[], double prodsVal[][]){
+        double acumulador = 0;
+        for(int i = 0; i < 3; i++){
+            System.out.printf("Producto: %s costo total: %.0f\n", prods[i], prodsVal[i][2]);
+            acumulador = acumulador + prodsVal[i][2];
+        }
+        System.out.println("---------------------------------");
+        System.out.println("El valor total de las existencias es: " + acumulador);
+    }
 }
